f8750b05348c530b72c1434b2468d3bf5d2482dc
mgandhi-1 <mgandhi@snolab.ca>
2024-10-21 06:59:43 -0400

    contains the config files for midas, not currently needed since using .bashrc for environment variables

---
+++ midas-config-files/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/echo You must source
+export MIDASSYS=$HOME/develop
+export MIDAS_OS=linux
+#export MIDASSYS=$HOME/daq/midas
+#export MIDAS_OS=darwin
+export MIDAS_EXPTAB=$HOME/develop/frontend_redpitaya/exptab
+export PATH=$PATH:$MIDASSYS/bin
+export MIDAS_EXPT_NAME=rnspc_exp
