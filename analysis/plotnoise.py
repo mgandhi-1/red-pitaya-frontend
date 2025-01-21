8b8e88a1fe1ca7ba8c25e90f21fcd84eec9d49a8
mgandhi-1 <mgandhi@snolab.ca>
2025-01-21 11:52:56 -0500

    changed plot size

--- analysis/plotnoise.py
+++ analysis/plotnoise.py
@@ -7,20 +7,20 @@ def load_json_data(file_path):
         data = json.load(file)
     return data
 
-# Extract the desired 4000 entries from the "RPDA" array in the JSON
+# Extract the desired 100 entries from the "DATA" array in the JSON
 def extract_and_convert_entries(data):
-    if "RPDA" in data:
-        adc_values = data["RPDA"][4000:20000]  # Extract first 4000 entries from the array
+    if "DATA" in data:
+        adc_values = data["DATA"][0:100]  # Extract first 100 entries from the array
         # Convert ADC values to voltage
-        voltage_values = [(2 * x) / 16384 for x in adc_values]
-        return voltage_values
+       # voltage_values = [(2 * x) / 16384 for x in adc_values]
+        return adc_values
     else:
-        raise KeyError("Key 'RPDA' not found in the JSON file.")
+        raise KeyError("Key 'DATA' not found in the JSON file.")
 
 # Plot the data
 def plot_data(data):
     plt.plot(data)
-    plt.title("First 4000 Entries from RPDA Array (in Voltage)")
+    plt.title("First 100 Entries from DATA Array (in Voltage)")
     plt.xlabel("Index")
     plt.ylabel("Voltage (V)")
     plt.grid(True)
